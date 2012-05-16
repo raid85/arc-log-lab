592d508d6a924763bcfcdb8ee5b4ace93791c0ab
nico.noel@gmail.com <nico.noel@gmail.com@5eef0262-f5f2-c081-4748-2841fd9bee37>
2012-05-16 03:25:06 +0000

    Modifier le MAIN pour tester le nouveau filtre qui enleve la severity passer en parametre...et regler les bugs de la derniere revision

--- trunk/src/ca/etsmtl/log430/lab1/SeverityLineRemover.java
+++ trunk/src/ca/etsmtl/log430/lab1/SeverityLineRemover.java
@@ -6,30 +6,33 @@ import java.io.PipedWriter;
 public class SeverityLineRemover extends Thread {
 	// Declarations
 
-	// Create local pipes to that will connect to upstream filters
+	String Severity;
+
+	// Maximum number of lines of text to be sorted
+	int MaxBufferSize = 100;
+
+	// Create local pipe that will connect to upstream filter
 	PipedReader InputPipe1 = new PipedReader();
-	PipedReader InputPipe2 = new PipedReader();
-
+	
 	// Create local pipes to that will connect to downstream filter
-	PipedWriter OutputPipe1 = new PipedWriter();
+	PipedWriter OutputPipe2 = new PipedWriter();
 
-	public SeverityLineRemover(PipedWriter InputPipe1, PipedWriter InputPipe2,
-			PipedWriter OutputPipe1) {
+	public SeverityLineRemover(String Severity, PipedWriter InputPipe1, PipedWriter OutputPipe1) {
+		this.Severity = Severity;
+		
 		try {
-			// Connect InputPipes to upstream filters
+			// Connect inputPipe to upstream filter
 			this.InputPipe1.connect(InputPipe1);
-			this.InputPipe2.connect(InputPipe2);
-			System.out.println("SeverityLineRemover:: connected to upstream filters.");
+			System.out.println("SeverityLineRemover:: connected to upstream filter.");
 		} catch (Exception Error) {
-			System.out.println("SeverityLineRemover:: Error connecting input pipes.");
-		} // try/catch
-
+			System.out.println("SeverityLineRemover:: Error connecting input pipe.");
+		} // catch
 		try {
 			// Connect outputPipe1 to downstream filter
-			this.OutputPipe1 = OutputPipe1;
-			System.out.println("MSeverityLineRemover:: connected to downstream filter.");
+			this.OutputPipe2 = OutputPipe1;
+			System.out.println("MergeFilter:: connected to downstream filter.");
 		} catch (Exception Error) {
-			System.out.println("SeverityLineRemover:: Error connecting output pipe.");
+			System.out.println("MergeFilter:: Error connecting output pipe.");
 		} // catch
 
 	} // Constructor
@@ -39,148 +42,87 @@ public class SeverityLineRemover extends Thread {
 
 	public void run() {
 		// Declarations
-		boolean Done1, Done2; // Flags for reading from each pipe
 
+		boolean Done1; // Flags for reading from pipe
 		// Begin process data from the input pipes
 		try {
 			// Declarations
 
-			// Need to be an array for easy conversion to string
+			// Needs to be an array for easy conversion to string
 			char[] CharacterValue1 = new char[1];
-			char[] CharacterValue2 = new char[1];
 
-			// Indicate when you are done reading on pipes #1 and #2
-			Done1 = false;
-			Done2 = false;
-
-			// integer values read from the pipes
-			int IntegerCharacter1;
-			int IntegerCharacter2;
-
-			// lines of text from input pipes #1 and #2
-			String LineOfText1 = "";
-			String LineOfText2 = "";
-
-			// Indicate whether lines of text are ready to be output
-			// to downstream filters
-			boolean Write1 = false;
-			boolean Write2 = false;
+			Done1 = false; // Indicates when you are done
+			// reading on pipe #1
+			int IntegerCharacter1; // integer value read from pipe
+			String LineOfText1 = ""; // line of text from inputpipe #1
+			boolean Write1 = false; // line of text to write to output
+			// pipe #1
 
 			// Main loop for reading data
 
-			while (!Done1 || !Done2) {
+			while (!Done1) {
 				// Read pipe #1
 				if (!Done1) {
 					IntegerCharacter1 = InputPipe1.read();
 					CharacterValue1[0] = (char) IntegerCharacter1;
-
 					if (IntegerCharacter1 == -1) // pipe #1 is closed
 					{
 						Done1 = true;
-						System.out
-								.println("MergeFilter:: Input pipe 1 closed.");
-
+						System.out.println("SeverityLineRemover:: input pipe closed.");
 						try {
 							InputPipe1.close();
 						} catch (Exception Error) {
 							System.out
-									.println("MergeFilter:: Error closing input pipe 1.");
+									.println("SeverityLineRemover:: Error closing input pipe.");
 						} // try/catch
-
 					} else {
-
 						if (IntegerCharacter1 == '\n') // end of line
 						{
-							System.out.println("MergeFilter:: Received: "
-									+ LineOfText1 + " on input pipe 1.");
+							System.out.println("SeverityLineRemover:: Received: "
+									+ LineOfText1 + " on input pipe.");
 							Write1 = true;
 						} else {
 							LineOfText1 += new String(CharacterValue1);
 						} // if
-
 					} // if ( IntegerCharacter1 == -1 )
-
 				} // if (!Done1)
-
-				// Read pipe #2
-				if (!Done2) {
-					IntegerCharacter2 = InputPipe2.read();
-					CharacterValue2[0] = (char) IntegerCharacter2;
-
-					if (IntegerCharacter2 == -1) // pipe #2 is closed
-					{
-						Done2 = true;
-						System.out
-								.println("MergeFilter:: input pipe 2 closed.");
-
-						try {
-							InputPipe2.close();
-						} catch (Exception Error) {
-							System.out
-									.println("MergeFilter:: Error closing input pipe 2.");
-						} // try/catch
-					} else {
-						if (IntegerCharacter2 == '\n') // end of line
-						{
-							System.out.println("MergeFilter:: Received: "
-									+ LineOfText2 + " on input pipe 2.");
-							Write2 = true;
-						} else {
-							LineOfText2 += new String(CharacterValue2);
-						} // if
-
-					} // if ( IntegerCharacter2 == -1 )
-
-				} // if (!Done2)
-
+				
 				if (Write1) {
 					Write1 = false;
-
-					try {
-						System.out.println("MergeFilter:: Sending "
-								+ LineOfText1 + " to output pipe.");
-						LineOfText1 += "\n";
-						OutputPipe1.write(LineOfText1, 0, LineOfText1.length());
-						OutputPipe1.flush();
-					} catch (Exception IOError) {
-						System.out.println("MergeFilter:: Write Error.");
-					} // try/catch
-
+					if(LineOfText1.substring(22,25).compareTo(Severity)!=0){
+						try {					
+							System.out.println("SeverityLineRemover:: Sending "
+									+ LineOfText1 + " to output pipe.");
+							LineOfText1 += "\n";
+							OutputPipe2.write(LineOfText1, 0, LineOfText1.length());
+							OutputPipe2.flush();
+							}
+						 catch (Exception IOError) {
+							
+							System.out.println("SeverityLineRemover:: Write Error."+ IOError+LineOfText1);
+						} // try/catch
+					}
 					LineOfText1 = "";
 
 				} // if ( Write1 )
 
-				if (Write2) {
-					Write2 = false;
-
-					try {
-						System.out.println("MergeFilter:: Sending "
-								+ LineOfText2 + " to output pipe.");
-						LineOfText2 += "\n";
-						OutputPipe1.write(LineOfText2, 0, LineOfText2.length());
-						OutputPipe1.flush();
-					} catch (Exception IOError) {
-						System.out.println("MergeFilter:: Write Error.");
-					} // try/catch
-
-					LineOfText2 = "";
-
-				} // if (Write2)
-
-			} // while ( !Done1 || !Done2 )
+			} // while (!Done1)
 
 		} // try
+		
+		
+		
 
 		catch (Exception Error) {
-			System.out.println("MergeFilter:: Interrupted.");
+			System.out.println("SeverityLineRemover:: Interrupted.");
 		} // catch
-
 		try {
-			System.out.println("MergeFilter:: output pipe closed.");
-			OutputPipe1.close();
+			System.out.println("SeverityLineRemover:: output pipe closed.");
+			OutputPipe2.close();
 		} catch (Exception Error) {
-			System.out.println("MergeFilter:: Error closing pipes");
+			System.out.println("SeverityLineRemover:: Error closing pipes");
 		} // try/catch
+		
 
 	} // run
 
